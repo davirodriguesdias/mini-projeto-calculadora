82737bc6f71576a9c47d206625be60cbad0ff5dd
Davi Rodrigues Dias <daviddias07032006@gmail.com>
2025-05-11 19:43:27 -0300

    Temas

--- scripts.js
+++ scripts.js
@@ -4,10 +4,17 @@ function insert(num) {
 
 function escuro() {
     document.querySelector('div#calculadora').style.backgroundColor = 'rgba(36, 35, 35, 0.9)'
-
-    
 }
 
 function claro() {
     document.querySelector('div#calculadora').style.backgroundColor = 'rgba(233, 230, 230, 0.9)'
 }
+
+function rosa() {
+    document.querySelector('div#calculadora').style.backgroundColor = 'pink'
+}
+
+function amarelo() {
+    document.querySelector('div#calculadora').style.backgroundColor = 'rgb(236, 236, 115)'
+}
+
